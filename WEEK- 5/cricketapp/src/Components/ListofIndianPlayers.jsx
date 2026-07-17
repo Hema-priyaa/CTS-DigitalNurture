52caea8305dab91b3193cd3c23fd13721e5d363e
Hemapriyaa <151140539+Hema-priyaa@users.noreply.github.com>
2026-07-17 09:37:42 +0530

    Delete WEEK- 5/cricketapp directory

--- WEEK- 5/cricketapp/src/Components/ListofIndianPlayers.jsx
+++
@@ -1,9 +0,0 @@
-export function ListofIndianPlayers({ IndianPlayers }) {
-  return (
-    IndianPlayers.map((item) => {
-      return (
-        <li key={item}>Mr. {item}</li>
-      );
-    })
-  );
-}
--- WEEK- 5/cricketapp/src/Components/OddPlayers.jsx
+++
@@ -1,9 +0,0 @@
-export function OddPlayers([first, , third, , fifth]) {
-  return (
-    <div>
-      <li> First : {first} </li>
-      <li> Third : {third} </li>
-      <li> Fifth : {fifth}</li>
-    </div>
-  );
-}
