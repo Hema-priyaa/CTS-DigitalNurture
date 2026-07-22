3c019932fca7893d8ef9c87ab29f0be9e8eedd14
Hemapriyaa <hemapriyaa230@gmail.com>
2026-07-22 21:44:51 +0530

    week 5 Exercise 12

---
+++ WEEK- 5/ticketbookingapp/src/Components/GuestGreeting.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GuestGreeting() {
+
+    return (
+
+        <div>
+
+            <h1>Please sign up.</h1>
+
+            <h3>Flight Details</h3>
+
+            <ul>
+                <li>Flight : AI202</li>
+                <li>From : Chennai</li>
+                <li>To : Delhi</li>
+                <li>Time : 10:00 AM</li>
+            </ul>
+
+            <p>Login to book your ticket.</p>
+
+        </div>
+
+    );
+
+}
+
+export default GuestGreeting;
---
+++ WEEK- 5/ticketbookingapp/src/Components/UserGreeting.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function UserGreeting() {
+
+    return (
+
+        <div>
+
+            <h1>Welcome back</h1>
+
+            <h3>Flight Details</h3>
+
+            <ul>
+                <li>Flight : AI202</li>
+                <li>From : Chennai</li>
+                <li>To : Delhi</li>
+                <li>Time : 10:00 AM</li>
+            </ul>
+
+            <button>Book Ticket</button>
+
+        </div>
+
+    );
+
+}
+
+export default UserGreeting;
